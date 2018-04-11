14fffb86fba15f7db86da5dd00732986bc674ed5
Elcoosp <elcoosp@gmail.com>
2018-04-11 23:31:43 +0200

    fix: typo

--- src/arrayize/index.js
+++ src/arrayize/index.js
@@ -2,7 +2,7 @@ const isArray = x => (Array.isArray ? Array.isArray(x) : v instanceof Array)
 
 /**
  * If x is undefined or null return an empty array, if it's already an array return it otherwise wrap it in an array
- * @param  {function} x The value to trnasform in an array or return
+ * @param  {function} x The value to transform in an array or return
  * @returns  {Array}
  */
 
