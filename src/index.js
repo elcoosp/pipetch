a7ac5d32f2366ccfebe5f79fb84ab50c87821403
Elcoosp <elcoosp@gmail.com>
2018-04-11 23:56:00 +0200

    add: module index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrayize: require('./arrayize'),
+  asyncPipe: require('./asyncPipe'),
+  composeM: require('./composeM'),
+  makeKeysMapper: require('./makeKeysMapper'),
+  mapKey: require('./mapKey'),
+  pipe: require('./pipe'),
+  pipetch: require('./pipetch'),
+  pipetchReduce: require('./pipetchReduce')
+}
