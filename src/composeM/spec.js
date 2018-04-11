b4b7c37c6cdcb2dd1acf5708000fabf7181def3b
Elcoosp <elcoosp@gmail.com>
2018-04-11 16:43:19 +0200

    test: composeM

---
+++ src/composeM/spec.js
@@ -0,0 +1,6 @@
+const composeM = require('./index')
+test('should return the accumulation of a chainable (e.g "then" here) call with the provided functions (arg 2) against the accumulator (arg 3)', () => {
+  composeM('then')(x => x + 2, x => x * 3)(Promise.resolve(3)).then(output =>
+    expect(output).toBe(15)
+  )
+})
