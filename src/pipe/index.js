7efcd4588fbf259dd14b5b8206dd34102306c1dc
Elcoosp <elcoosp@gmail.com>
2018-04-11 17:15:58 +0200

    doc: refix pipe

--- src/pipe/index.js
+++ src/pipe/index.js
@@ -1,6 +1,6 @@
 /**
  * @param  {...function} fns - Functions to execute against initAcc
- * @returns  {(initAcc: *) => *} - Function taking the initial value provided to the accumulator
+ * @returns  {(initAcc: *) => *} Function taking the initial value provided to the accumulator
  */
 const pipe = (...fns) => initAcc => fns.reduce((acc, f) => f(acc), initAcc)
 module.exports = pipe
