dd45c47a7d22b5063655ff946e1ecdcde1da0c24
Elcoosp <elcoosp@gmail.com>
2018-04-11 22:20:37 +0200

    test: pipetchReduce

---
+++ src/pipetchReduce/spec.js
@@ -0,0 +1,21 @@
+const pipetchReduce = require('./index.js')
+
+test('should call the fetcher promise-aware function with mockData, and apply the provided functions', () => {
+  const fetcher = d => Promise.resolve(d)
+  const mockData = JSON.stringify([
+    { id: 2, name: 'bar' },
+    { id: 3, name: 'plop' }
+  ])
+
+  const output = pipetchReduce(fetcher)({
+    id: x => x * 2,
+    name: x => 'new name'
+  })(mockData)
+
+  output.then(data => {
+    expect(data).toEqual([
+      { id: 4, name: 'new name' },
+      { id: 6, name: 'new name' }
+    ])
+  })
+})
