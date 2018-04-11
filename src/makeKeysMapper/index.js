cd58197771ac95e50c51b92a46e25c07b8c6b512
Elcoosp <elcoosp@gmail.com>
2018-04-11 18:42:07 +0200

    add: makeKeysMapper

---
+++ src/makeKeysMapper/index.js
@@ -0,0 +1,16 @@
+const mapKey = require('../mapKey')
+
+/**
+ * Take an object and return an array of corresponding mapKey
+ * @param  {Object} mapKeyObject - An object with key/function tuple to map
+ * @returns {[...function]} An array of mapKey functions ready to execute
+ */
+const makeKeysMapper = mapKeyObject =>
+  Object.entries(mapKeyObject).reduce(
+    (acc, [k, f]) => [...acc, mapKey(k)(f)],
+    []
+  )
+
+makeKeysMapper({ g: 6, f: 4 })
+
+module.exports = makeKeysMapper
