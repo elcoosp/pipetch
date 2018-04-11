0e5ee0c2bd023dac56921b29265af04c406b5f1d
Elcoosp <elcoosp@gmail.com>
2018-04-11 22:30:23 +0200

    doc: pipetchReduce

--- src/pipetchReduce/index.js
+++ src/pipetchReduce/index.js
@@ -7,6 +7,13 @@ const json = res => JSON.parse(res)
 const map = mapper => arr => arr.map(mapper)
 const arrayize = x => (!x ? [] : Array.isArray(x) ? x : [x])
 
+/**
+ * @param  {function} fetcher Fetch provider promise-aware function
+ * @param  {Object} keysMapper Object of tuple key mapping function
+ * @param  {...function} [transformers] Optionnal transformers applied to the whole list retrieved
+ * @param  {*} fetchArgs Fetching args from the fetcher function
+ * @returns  {(keysMapper: Object, transformers: ...function) => (fetchArgs: *) => Promise}
+ */
 const pipetchReduce = fetcher => (keysMapper, ...transformers) => (
   ...fetchArgs
 ) =>
